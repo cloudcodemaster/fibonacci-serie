a353c6ff5c1ea4754944e4683696f335d2e4e280
cloudcodepuppet <tono_novoa@hotmail.com>
2024-06-10 17:00:16 -0600

    Fibonacci console app
    Console App

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter the number of terms: ");
+ulong terms = ulong.Parse(Console.ReadLine());
+Console.ReadKey();
+
+// Call the method to print the Fibonacci series
+PrintFibonacciSeries(terms);
+
+
+    static void PrintFibonacciSeries(ulong terms)
+{
+    ulong firstNumber = 0, secondNumber = 1, nextNumber;
+
+    Console.WriteLine("Fibonacci Series:");
+
+    for (ulong i = 1; i <= terms; i++)
+    {
+        Console.Write(firstNumber + " ");
+        nextNumber = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = nextNumber;
+    }
+}
